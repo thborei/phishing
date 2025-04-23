f4023e48cc737f0d9a076f9b532254fca4780490
thborei <thborei@icloud.com>
2025-04-23 10:58:58 +0200

    changement des formulaires au niveau creation

--- view/campaigns/formulaire/form.php
+++ view/campaigns/formulaire/form.php
@@ -7,21 +7,21 @@
 <div class="container">
     <form action="<?= !isset($field) ? '/campaigns/formulaire/create' : '/campaigns/formulaire/update/'.$field->getId() ?>" method="post">
         <div class="form-group row">
-            <label for="url" class="col-sm-2 col-form-label">URL</label>
+            <label for="url" class="col-sm-2 col-form-label">Nome du Formulaire</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="url" placeholder="URL de la campagne" name="url" value="<?= isset($campaign) ? htmlspecialchars($campaign->getUrl()) : '' ?>">
+                <input type="text" class="form-control" id="name" placeholder="nom du field" name="name" value="<?= isset($field) ? htmlspecialchars($field->getName()) : '' ?>">
             </div>
         </div>
         <div class="form-group">
             <label for="type">Type de formulaire</label>
             <select class="form-control" id="type" name="type">
-                <option value="pre-defined" <?= isset($campaign) && $campaign->getType() === 'pre-defined' ? 'selected' : '' ?>>Pré-définie</option>
-                <option value="custom" <?= isset($campaign) && $campaign->getType() === 'custom' ? 'selected' : '' ?>>Personnalisée</option>
+                <option value="date" <?= isset($field) && $field->getType() === 'date' ? 'selected' : '' ?>>Date</option>
+                <option value="email" <?= isset($field) && $field->getType() === 'email' ? 'selected' : '' ?>>email</option>
             </select>
         </div>
         <div class="form-group row">
             <div class="col-sm-10">
-                <button type="submit" class="btn btn-primary"><?= isset($campaign) ? 'Modifier' : 'Créer' ?></button>
+                <button type="submit" class="btn btn-primary"><?= isset($field) ? 'Modifier' : 'Créer' ?></button>
             </div>
         </div>
     </form>
