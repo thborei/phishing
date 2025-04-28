9a8fc00d8131e5618338719ce1786b2a2ff1df9f
thborei <thborei@icloud.com>
2025-04-28 14:30:40 +0200

    fix

--- view/indexView.php
+++ view/indexView.php
@@ -1,6 +1,7 @@
+<?= session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
-<?= session_start() ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
