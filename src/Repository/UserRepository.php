5066dd580516178ff96d367d08d1d406f052a169
thborei <thborei@icloud.com>
2025-04-28 14:19:32 +0200

    debug

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -55,6 +55,7 @@ class UserRepository
         if ($user && password_verify($password, $user['password_login'])) {
             session_start();
             $_SESSION['user_id'] = $user['id'];
+            die(var_dump($_SESSION['user_id']));
             header('Location: /acceuil');
         } else {
             die("Mauvais identifiants !");
