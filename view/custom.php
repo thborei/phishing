bd78c392b790097daaf74282b58a0d4a1ff06caa
thborei <thborei@icloud.com>
2025-05-26 11:46:31 +0200

    rajout de name

--- view/custom.php
+++ view/custom.php
@@ -12,9 +12,9 @@
     <h1>Formulaire</h1>
     <form action="/custom/create" method="post">
     <p>Veuillez remplir tous les champs</p>
-    <input type="id_camp" style="display: none;" value=" <= $id_camp ?>" name="id_camp" />
+    <input type="hidden" value=" <= $id_camp ?>" name="id_camp" />
     <?php foreach ($fields as $field): ?>
-      <input type="<?= $field->getType()?>" placeholder="<?= htmlspecialchars($field->getName()) ?>" />
+      <input type="<?= $field->getType()?>" name="<? $field->getType()?>" placeholder="<?= htmlspecialchars($field->getName()) ?>" required />
     <?php endforeach; ?>
     <button type="submit">Connexion</button>
     </form>
