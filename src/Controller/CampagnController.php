5762df6c9b57d070d87d3508f1f2db81cf29b613
Theo Boyet <theo1638@gmail.com>
2025-06-12 23:15:42 +0200

    fix

--- src/Controller/CampagnController.php
+++ src/Controller/CampagnController.php
@@ -55,17 +55,27 @@ class CampagnController
                 echo "Veuillez remplir tous les champs.";
                 return;
             }
-            if (empty($users) && !empty($service)) {
-            $users = $this->userRepository->getUsersByService($service);
+             elseif (empty($users)) {
+                echo "Veuillez sélectionner au moins un utilisateur.";
+                return;
             }
-            var_dump($users);
-            $this->repository->createCampagn($type, $url, $predifine, $active, $displayed);
-            foreach ($users as $userId) {
+            if (!empty($users) && empty($service)) {
+                foreach ($users as $userId) {
                 $this->userRepository->getUser($userId)->EnvoieMail();
             }
+            }
+            if (empty($users) && !empty($service)) {
+            $users = $this->userRepository->getUsersByService($service);
+            foreach ($users as $user) {
+                $user -> EnvoieMail();
+            }
+
+            $this->repository->createCampagn($type, $url, $predifine, $active, $displayed);
+            
             header('Location: /campaigns'); // Redirection après l'enregistrement
             exit;
-        } else {
+        }
+    } else {
             $users = $this->userRepository->getUsers();
             $services = $this->userRepository->getServices();
             $contenu = $this->moteur->render('campaigns/form', ['users' => $users, 'services' => $services]);
