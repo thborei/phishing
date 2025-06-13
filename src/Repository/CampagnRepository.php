ccb7cded64df9e44d01fed20e1e772d707222c3c
thborei <thborei@icloud.com>
2025-06-13 10:37:33 +0200

    fix tabpe

--- src/Repository/CampagnRepository.php
+++ src/Repository/CampagnRepository.php
@@ -39,8 +39,8 @@ class CampagnRepository
             $row['type_campagn'],
             $row['url_campagn'],
             $row['predefine_campagn'] ?? null,
-            $row['active'],
-            $row['displayed']
+            $row['active_campagn'] ?? true,
+            $row['displayed_campagn']
         )  ,$results);
     }
 
@@ -56,20 +56,20 @@ class CampagnRepository
             $row['type_campagn'],
             $row['url_campagn'],
             $row['predefine_campagn'] ?? null,
-            $row['active'],
-            $row['displayed']
+            $row['active_campagn'],
+            $row['displayed_campagn']
         );
     }
 
     public function createCampagn(string $type, string $url, ?string $predifine, ?bool $active, ?bool $displayed): void
     {
-        $stmt = $this->pdo->prepare("INSERT INTO CAMPAGNS (type_campagn, url_campagn, predefine_campagn, active, displayed) VALUES (:type, :url, :predifine, :active, :displayed)");
+        $stmt = $this->pdo->prepare("INSERT INTO CAMPAGNS (type_campagn, url_campagn, predefine_campagn, active_campagn, displayed_campagn) VALUES (:type, :url, :predifine, :active, :displayed)");
         $stmt->execute([':type' => $type, ':url' => $url, ':predifine' => $predifine, ':active' => $active ? 1 : 0, ':displayed' => $displayed ? 1 : 0]);
     }
 
     public function updateCampagn(int $id, string $type, string $url, ?bool $active, ?bool $displayed): void
     {
-        $stmt = $this->pdo->prepare("UPDATE CAMPAGNS SET type_campagn = :type, url_campagn = :url, active = :active, displayed = :displayed WHERE id_campagn = :id");
+        $stmt = $this->pdo->prepare("UPDATE CAMPAGNS SET type_campagn = :type, url_campagn = :url, active_campagn = :active, displayed_campagn = :displayed WHERE id_campagn = :id");
         $stmt->execute([':id' => $id, ':type' => $type, ':url' => $url, ':active' => $active ? 1 : 0, ':displayed' => $displayed ? 1 : 0]);
     }
 
@@ -118,8 +118,8 @@ class CampagnRepository
                 $row['type_campagn'],
                 $row['url_campagn'],
                 $row['predefine_campagn'] ?? null,
-                $row['active'],
-                $row['displayed']
+                $row['active_campagn'] ?? true,
+                $row['displayed_campagn']
             );
         }
         return null;
@@ -132,7 +132,7 @@ class CampagnRepository
 
     public function getDisplayedCampaign() 
     {
-        $stmt = $this->pdo->prepare("SELECT * FROM CAMPAGNS WHERE displayed = 1 AND active = 1 LIMIT 1");
+        $stmt = $this->pdo->prepare("SELECT * FROM CAMPAGNS WHERE displayed_campagn = 1 AND active_campagn = 1 LIMIT 1");
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -142,8 +142,8 @@ class CampagnRepository
             $result['type_campagn'],
             $result['url_campagn'],
             $result['predefine_campagn'],
-            $result['active'],
-            $result['displayed']
+            $result['active_campagn'],
+            $result['displayed_campagn']
         );
     }
 }
