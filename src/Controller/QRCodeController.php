ee0995dd63ed17c40a40fcab9d514f110bfba145
thborei <thborei@icloud.com>
2025-06-13 14:34:19 +0200

    suivi de test

--- src/Controller/QRCodeController.php
+++ src/Controller/QRCodeController.php
@@ -15,18 +15,23 @@ class QRCodeController
         $this->moteur = new MoteurDeRendu();
         $this->campaignRepository = new CampagnRepository();
     }
-    public function get()
-    {
+ public function get()
+{
     $campaign = $this->campaignRepository->getDisplayedCampaign();
     $campaign->createQrcode($campaign->getUrl(), $campaign->getId());
 
-    $hexData = $campaign->getHex();
+    $hexData = $campaign->getHex(); // chaîne hex textuelle, ex: "FF00A1..."
 
-    header('Content-Type: text/plain');
-    header('Content-Length: ' . strlen($hexData));
+    // Supposons que $hexData soit une chaîne hex sans espaces ni préfixes 0x
+    // Si ce n'est pas le cas, il faudra nettoyer $hexData avant
+    $binaryData = hex2bin($hexData);
+
+    header('Content-Type: application/octet-stream');
+    header('Content-Length: ' . strlen($binaryData));
     header('Connection: close');
 
-    echo $hexData;
+    echo $binaryData;
     exit;
-    }
+}
+
 }
